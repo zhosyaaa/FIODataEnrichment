2c2b18fa6044f810b4208404766535fd979e925c
zhosyaaa <musabecova05@gmail.com>
2023-09-25 23:54:13 +0600

    graphql

---
+++ internal/graphql/resolver.go
@@ -0,0 +1,94 @@
+package graphql
+
+import (
+	"TestCase/internal/models"
+	"TestCase/internal/repository"
+	"TestCase/internal/services"
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Resolver struct {
+	PersonRepository  repository.PersonRepositoryImpl
+	EnrichmentService services.EnrichmentService
+}
+
+func NewResolver(personRepository repository.PersonRepositoryImpl) *Resolver {
+	return &Resolver{PersonRepository: personRepository}
+}
+
+func (r *Resolver) resolvePersons(ctx context.Context) ([]models.Person, error) {
+	persons, err := r.PersonRepository.GetAllPersons()
+	if err != nil {
+		return nil, err
+	}
+	return persons, nil
+}
+
+func (r *Resolver) resolvePerson(ctx context.Context, ID int) (*models.Person, error) {
+	person, err := r.PersonRepository.GetPersonByID(uint(ID))
+	if err != nil {
+		return nil, err
+	}
+	return person, nil
+}
+
+func (r *Resolver) resolveFilteredPersons(ctx context.Context, args struct {
+	Gender  *string
+	Age     *int
+	Page    *int
+	PerPage *int
+}) ([]models.Person, error) {
+	filteredPersons, err := r.PersonRepository.FilterPersons(
+		*args.Gender, *args.Age, *args.Page, *args.PerPage)
+	if err != nil {
+		return nil, err
+	}
+	return filteredPersons, nil
+}
+
+func (r *Resolver) resolveCreatePerson(ctx context.Context, Input models.Input) (*models.Person, error) {
+	r.EnrichmentService = *services.NewEnrichmentService(
+		&http.Client{},
+		&http.Client{},
+		&http.Client{},
+		&r.PersonRepository,
+		make(chan string),
+		nil,
+	)
+	newPerson := &models.Person{
+		Name:       Input.Name,
+		Surname:    Input.Surname,
+		Patronymic: Input.Patronymic,
+	}
+	fio := fmt.Sprintf("%s %s %s", newPerson.Name, newPerson.Surname, newPerson.Patronymic)
+	r.EnrichmentService.FIOChannel <- fio
+	return newPerson, nil
+}
+
+func (r *Resolver) resolveUpdatePerson(ctx context.Context, ID int, Input models.Person) (*models.Person, error) {
+	person, err := r.PersonRepository.GetPersonByID(uint(ID))
+	if err != nil {
+		return nil, err
+	}
+
+	person.Name = Input.Name
+	person.Surname = Input.Surname
+	person.Patronymic = Input.Patronymic
+	person.Age = Input.Age
+	person.Gender = Input.Gender
+	person.Nationality = Input.Nationality
+
+	if err := r.PersonRepository.UpdatePerson(person); err != nil {
+		return nil, err
+	}
+	return person, nil
+}
+
+func (r *Resolver) resolveDeletePerson(ctx context.Context, ID int) (bool, error) {
+	if err := r.PersonRepository.DeletePerson(ID); err != nil {
+		return false, err
+	}
+	return true, nil
+}
---
+++ internal/graphql/schema.go
@@ -0,0 +1,189 @@
+package graphql
+
+import (
+	"TestCase/internal/models"
+	"TestCase/internal/repository"
+	"errors"
+	"github.com/graphql-go/graphql"
+	"strconv"
+)
+
+var (
+	personType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Person",
+		Fields: graphql.Fields{
+			"id":          &graphql.Field{Type: graphql.ID},
+			"name":        &graphql.Field{Type: graphql.String},
+			"surname":     &graphql.Field{Type: graphql.String},
+			"patronymic":  &graphql.Field{Type: graphql.String},
+			"age":         &graphql.Field{Type: graphql.Int},
+			"gender":      &graphql.Field{Type: graphql.String},
+			"nationality": &graphql.Field{Type: graphql.String},
+		},
+	})
+
+	queryType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"persons": &graphql.Field{
+				Type: graphql.NewList(personType),
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+					return resolver.resolvePersons(p.Context)
+				},
+			},
+			"person": &graphql.Field{
+				Type: personType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.ID)},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					id := p.Args["id"].(string)
+					idValue, err := strconv.Atoi(id)
+					if err != nil {
+						return nil, err
+					}
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+					return resolver.resolvePerson(p.Context, idValue)
+				},
+			},
+			"filteredPersons": &graphql.Field{
+				Type: graphql.NewList(personType),
+				Args: graphql.FieldConfigArgument{
+					"gender":  &graphql.ArgumentConfig{Type: graphql.String},
+					"age":     &graphql.ArgumentConfig{Type: graphql.Int},
+					"page":    &graphql.ArgumentConfig{Type: graphql.Int},
+					"perPage": &graphql.ArgumentConfig{Type: graphql.Int},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					gender, _ := p.Args["gender"].(string)
+					age, _ := p.Args["age"].(int)
+					page, _ := p.Args["page"].(int)
+					perPage, _ := p.Args["perPage"].(int)
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+					args := struct {
+						Gender  *string
+						Age     *int
+						Page    *int
+						PerPage *int
+					}{
+						Gender:  &gender,
+						Age:     &age,
+						Page:    &page,
+						PerPage: &perPage,
+					}
+					return resolver.resolveFilteredPersons(p.Context, args)
+				},
+			},
+		},
+	})
+
+	mutationType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"createPerson": &graphql.Field{
+				Type: personType,
+				Args: graphql.FieldConfigArgument{
+					"input": &graphql.ArgumentConfig{Type: InputType},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					input := p.Args["input"].(models.Input)
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+
+					return resolver.resolveCreatePerson(p.Context, input)
+				},
+			},
+			"updatePerson": &graphql.Field{
+				Type: personType,
+				Args: graphql.FieldConfigArgument{
+					"id":    &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.ID)},
+					"input": &graphql.ArgumentConfig{Type: personType},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+					id, idOK := p.Args["id"].(string)
+					input, inputOK := p.Args["input"].(map[string]interface{})
+					if !idOK || !inputOK {
+						return nil, errors.New("Invalid argument types")
+					}
+					idValue, err := strconv.Atoi(id)
+					person, err := resolver.resolvePerson(p.Context, idValue)
+					if err != nil {
+						return nil, err
+					}
+					if err != nil {
+						return nil, err
+					}
+					if name, ok := input["name"].(string); ok {
+						person.Name = name
+					}
+					if surname, ok := input["surname"].(string); ok {
+						person.Surname = surname
+					}
+					if patronymic, ok := input["patronymic"].(string); ok {
+						person.Patronymic = patronymic
+					}
+					if age, ok := input["age"].(int); ok {
+						person.Age = age
+					}
+					if gender, ok := input["gender"].(string); ok {
+						person.Gender = gender
+					}
+					if nationality, ok := input["nationality"].(string); ok {
+						person.Nationality = nationality
+					}
+					updatedPerson, err := resolver.resolveUpdatePerson(p.Context, idValue, *person)
+					if err != nil {
+						return nil, err
+					}
+
+					return updatedPerson, nil
+				},
+			},
+			"deletePerson": &graphql.Field{
+				Type: graphql.Boolean,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.ID)},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					id, idOK := p.Args["id"].(string)
+					if !idOK {
+						return false, errors.New("Invalid argument types")
+					}
+					idValue, err := strconv.Atoi(id)
+					if err != nil {
+						return false, err
+					}
+
+					resolver := NewResolver(p.Context.Value("personRepository").(repository.PersonRepositoryImpl))
+					success, err := resolver.resolveDeletePerson(p.Context, idValue)
+					if err != nil {
+						return false, err
+					}
+
+					return success, nil
+				},
+			},
+		},
+	})
+)
+
+var InputType = graphql.NewInputObject(graphql.InputObjectConfig{
+	Name: "Input",
+	Fields: graphql.InputObjectConfigFieldMap{
+		"name": &graphql.InputObjectFieldConfig{
+			Type: graphql.String,
+		},
+		"surname": &graphql.InputObjectFieldConfig{
+			Type: graphql.String,
+		},
+		"patronymic": &graphql.InputObjectFieldConfig{
+			Type: graphql.String,
+		},
+	},
+})
+
+var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query:    queryType,
+	Mutation: mutationType,
+})
